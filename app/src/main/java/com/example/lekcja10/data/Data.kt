458401a65776a31c63b4e40b62cda40089bea200
Wiktor <wiktor-wosiek@wp.pl>
2025-11-17 14:15:26 +0100

    jeszcze cos do menu

--- app/src/main/java/com/example/lekcja10/data/Data.kt
+++ app/src/main/java/com/example/lekcja10/data/Data.kt
@@ -11,7 +11,22 @@ class Data {
                 type = ItemType.SOUP,
                 price = 15.0,
                 image = R.drawable.pomidorowa
-            )
+            ),
+            Item(
+                name = "Rosół",
+                description = "Z wczoraj",
+                type = ItemType.SOUP,
+                price = 12.30,
+                image = R.drawable.rosol
+            ),
+            Item(
+                name = "Ogórkowa",
+                description = "Sam kisiłem ogóra",
+                type = ItemType.SOUP,
+                price = 16.73,
+                image = R.drawable.pomidorowa
+            ),
+            
         )
     }
 }
